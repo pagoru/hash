aa704465b8762ce8738c088640187235a924ea57
pagoru <pagoru@gmail.com>
2016-10-05 15:53:23 +0200

    Better versión with options

--- hash01/Program.cs
+++ hash01/Program.cs
@@ -12,27 +12,66 @@ namespace hash01
     {
         static void Main(string[] args)
         {
-            String textIn = null;
-            Console.Write("Entra text: ");
-            while (textIn==null)
-                textIn = Console.ReadLine();
 
-            // Convertim l'string a un array de bytes
-            byte[] bytesIn = UTF8Encoding.UTF8.GetBytes(textIn);
-            // Instanciar classe per fer hash
+            Console.WriteLine("Programa iniciat.");
+            while (true)
+            {
+                Console.WriteLine("[0] Calcular el Hash");
+                Console.WriteLine("[1] Verificar un hash");
+                Console.WriteLine("[2] Sortir");
+
+                String option = readLine("Introudeix una de les opcions:");
+                
+                if (option == "0")
+                {
+                    String path = readLine("Introdueix la ruta de l'arxiu: ");
+                    String text = System.IO.File.ReadAllText(@path);
+                    String pathHash = readLine("Introdueix la ruta on vols guardar el hash: ");
+                    System.IO.File.WriteAllText(@pathHash, getHash(text));
+
+                }
+                else if (option == "1")
+                {
+                    String path = readLine("Introdueix la ruta de l'arxiu que conté el text: ");
+                    String text = System.IO.File.ReadAllText(@path);
+                    String pathHash = readLine("Introdueix la ruta de l'arxiu que conté el hash: ");
+                    String hash = System.IO.File.ReadAllText(@pathHash);
+
+                    String hashText = getHash(text);
+
+                    Console.WriteLine((String.Equals(hash, hashText)) ? "El hash es correcte." : "El hash es incorrecte.");
+
+                }
+                else if (option == "2")
+                {
+                    Console.WriteLine("Has sortit.");
+                    break;
+                } else
+                {
+                    Console.WriteLine("Introdueix una opció valida.");
+                }
+            }
+
+        }
+
+        static String getHash(String text)
+        {
+            byte[] bytesIn = UTF8Encoding.UTF8.GetBytes(text);
             SHA512Managed SHA512 = new SHA512Managed();
-            // Calcular hash
             byte[] hashResult = SHA512.ComputeHash(bytesIn);
-
-            // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
-            // cal convertir-lo a un string
-
-            String textOut = BitConverter.ToString(hashResult, 0);
-            Console.WriteLine("Hash {0}", textOut);
-            Console.ReadKey();
-
-            // Eliminem la classe instanciada
             SHA512.Dispose();
+            return BitConverter.ToString(hashResult, 0);
+        }
+
+        static String readLine(String sendText)
+        {
+            String text = null;
+            Console.WriteLine(sendText);
+            while (text == null)
+            {
+                text = Console.ReadLine();
+            }
+            return text;
         }
     }
 }
